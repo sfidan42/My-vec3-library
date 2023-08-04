752e6f48c227ee4dbfbe790b49d7a705d40d0251
sadettin <sadettinfidan00@gmail.com>
2023-08-04 14:05:45 +0300

    update libft

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfidan <sfidan@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/23 10:42:40 by sfidan            #+#    #+#             */
+/*   Updated: 2022/07/26 13:23:14 by sfidan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	i;
+	size_t	j;
+	size_t	size;
+	char	*s;
+
+	if (!s1 || !s2)
+		return (0);
+	size = ft_strlen(s1) + ft_strlen(s2) + 1;
+	s = (char *)malloc(size);
+	if (!s)
+		return (0);
+	i = 0;
+	j = 0;
+	while (s1[i])
+		s[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		s[j++] = s2[i++];
+	s[j] = 0;
+	return (s);
+}
